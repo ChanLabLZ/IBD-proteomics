06b0e612a40cfe8711bfbc54405538bbd6148981
FW <fanwu1213@163.com>
2026-08-17 13:20:43 +0800

    update clinical information

---
+++ Clinical_Information/plot_proteinNumber.R
@@ -0,0 +1,51 @@
+
+rm(list=ls())
+setwd("~/Documents/work/20240816_LiangZhu/20251124_PrMetabolism_XHOM_WF/")
+
+library(dplyr)
+library(readxl)
+
+protein_counts <- read_excel("./Allsample_ProteinIdentification.2.xlsx")
+protein_counts <- as.data.frame(protein_counts)
+
+protein_counts$Group2 <- "NotKnow"
+protein_counts[protein_counts$Group == 'UNT',]$Group2 <- 'UNT'
+protein_counts[protein_counts$Group == 'HC',]$Group2 <- 'HC'
+protein_counts[protein_counts$Group == 'NR' & 
+                   protein_counts$Description == 'anti-TNF',]$Group2 <- 'a1NR'
+protein_counts[protein_counts$Group == 'R' & 
+                protein_counts$Description == 'anti-TNF',]$Group2 <- 'a1R'
+protein_counts[protein_counts$Group == 'NR' & 
+                protein_counts$Description == 'anti-p40',]$Group2 <- 'a2NR'
+protein_counts[protein_counts$Group == 'R' & 
+                protein_counts$Description == 'anti-p40',]$Group2 <- 'a2R'
+
+protein_counts <- protein_counts[order(protein_counts$Group2, protein_counts$`Identified proteins`),]
+protein_counts$Rank <- c(1:82,1:66,1:53,1:48,1:101,1:153)
+
+protein_counts <- protein_counts[order(protein_counts$Group, protein_counts$`Identified proteins`),]
+protein_counts$Rank2 <- c(1:101,1:135,1:114,1:153)
+
+
+library(RColorBrewer)
+color_manual = c(brewer.pal(9,"Set1"))
+color_manual <- c("#377EB8","#FF7F00","#984EA3", "#A65628", "#E41A1C", "#999999", "#F781BF","#4DAF4A")
+
+library(ggplot2)
+p1<-ggplot(protein_counts, aes(Rank, `Identified proteins`, col = Group2)) +
+    geom_point() + geom_line() +
+    theme_wf +
+    scale_color_manual(values = color_manual)
+    
+p2<-ggplot(protein_counts, aes(Rank2, `Identified proteins`, col = Group)) +
+    geom_point() + geom_line() +
+    theme_wf +
+    scale_color_manual(values = color_manual)
+
+p1+p2
+
+pdf("./XHOM_PM_sampleInfor.3.pdf",9.5,8)
+print(p1)
+print(p2)
+dev.off()
+
---
+++ Clinical_Information/plot_sampleInformation.R
@@ -0,0 +1,132 @@
+
+rm(list=ls())
+setwd("~/Documents/work/20240816_LiangZhu/20251124_PrMetabolism_XHOM_WF/")
+
+library(dplyr)
+library(readxl)
+
+model_excel <- read_excel("./XHOM_PM_sampleInfor.xlsx")
+model_excel <- as.data.frame(model_excel)
+
+## counts ------------
+table(model_excel$Group, model_excel$Description)
+
+model_excel <- model_excel[,c(1:35)]
+
+
+## plot ---------
+
+sampleInfor <- model_excel
+
+sampleInfor$Group2 <- "NotKnow"
+sampleInfor[sampleInfor$Group == 'UNT',]$Group2 <- 'UNT'
+sampleInfor[sampleInfor$Group == 'HC',]$Group2 <- 'HC'
+sampleInfor[sampleInfor$Group == 'NR' & 
+                sampleInfor$Description == 'anti-TNF',]$Group2 <- 'a1NR'
+sampleInfor[sampleInfor$Group == 'R' & 
+                sampleInfor$Description == 'anti-TNF',]$Group2 <- 'a1R'
+sampleInfor[sampleInfor$Group == 'NR' & 
+                sampleInfor$Description == 'anti-p40',]$Group2 <- 'a2NR'
+sampleInfor[sampleInfor$Group == 'R' & 
+                sampleInfor$Description == 'anti-p40',]$Group2 <- 'a2R'
+
+
+sampleInfo <- sampleInfor[!sampleInfor$Group %in% c("HC"),]
+rm(sampleInfor)
+sampleInfo$Group2 <- factor(sampleInfo$Group2, levels = c("UNT","a1NR","a1R","a2NR","a2R"))
+
+
+# for (i in c("UNT","a1NR","a1R","a2NR","a2R")) {
+#     temp <- sampleInfo[sampleInfo$Group2==i,]
+#     suT <- summary(temp$Hb)
+#     cil <- suT[2]
+#     ciu <- suT[5]
+#     m <- suT[4]
+#     print(paste0(m,' (',cil,'–',ciu,')'))
+# }
+
+(p0 <- ggplot(sampleInfo, aes(Group2, `SES-CD(<3)`)) +
+    geom_boxplot(na.rm = T, outlier.shape = NA) +
+    geom_jitter(width = 0.2, alpha = 1, size = 1, color = "darkred") +
+        ggpubr::stat_compare_means(
+            comparisons = list(c("a2NR","a2R"),c("a1NR", "a1R")),
+            na.rm = TRUE,
+            method = "wilcox.test",  
+            label = "p.format",      
+            label.x.npc = "center",  
+            label.y.npc = "top",     
+            vjust = -0.5             
+        ) + 
+    theme_wf2 +
+    theme(axis.title.x = element_blank()))
+
+
+(p1 <- ggplot(sampleInfo, aes(Group2, `CRP(<10)`)) +
+    geom_boxplot(na.rm = T, outlier.shape = NA) +
+    geom_jitter(width = 0.2, alpha = 1, size = 1, color = "darkred") +
+        ggpubr::stat_compare_means(
+            comparisons = list(c("a2NR","a2R"),c("a1NR", "a1R")),
+            na.rm = TRUE,
+            method = "wilcox.test",  
+            label = "p.format",      
+            label.x.npc = "center",  
+            label.y.npc = "top",     
+            vjust = -0.5             
+        ) + 
+    theme_wf2 + ylim(0,130)+
+    theme(axis.title.x = element_blank()))
+
+
+(p2 <- ggplot(sampleInfo, aes(Group2, `FCCP(<250)`)) +
+    geom_boxplot(na.rm = T, outlier.shape = NA) +
+    geom_jitter(width = 0.2, alpha = 1, size = 1, color = "darkred") +
+        ggpubr::stat_compare_means(
+            comparisons = list(c("a2NR","a2R"),c("a1NR", "a1R")),
+            na.rm = TRUE,
+            method = "wilcox.test",  
+            label = "p.format",      
+            label.x.npc = "center",  
+            label.y.npc = "top",     
+            vjust = -0.5             
+        ) + 
+    theme_wf2 + ylim(0,5000)+
+    theme(axis.title.x = element_blank()))
+
+
+(p3 <- ggplot(sampleInfo, aes(Group2,`ESR`)) + 
+    geom_boxplot(na.rm = T, outlier.shape = NA) +
+    geom_jitter(width = 0.2, alpha = 1, size = 1, color = "darkred") +
+        ggpubr::stat_compare_means(
+            comparisons = list(c("a2NR","a2R"),c("a1NR", "a1R")),
+            na.rm = TRUE,
+            method = "wilcox.test",  
+            label = "p.format",      
+            label.x.npc = "center",  
+            label.y.npc = "top",     
+            vjust = -0.5             
+        ) + 
+    theme_wf2 +
+    theme(axis.title.x = element_blank()))
+
+
+(p4 <- ggplot(sampleInfo, aes(Group2,`Hb`)) + 
+    geom_boxplot(na.rm = T, outlier.shape = NA) +
+    geom_jitter(width = 0.2, alpha = 1, size = 1, color = "darkred") +
+        ggpubr::stat_compare_means(
+            comparisons = list(c("a2NR","a2R"),c("a1NR", "a1R")),
+            na.rm = TRUE,
+            method = "wilcox.test",  
+            label = "p.format",      
+            label.x.npc = "center",  
+            label.y.npc = "top",     
+            vjust = -0.5             
+        ) + 
+    theme_wf2 +
+    theme(axis.title.x = element_blank()))
+
+
+
+pdf("./XHOM_PM_sampleInfor.2.pdf",16,8)
+p0+p1+p2+p3+p4
+dev.off()
+
